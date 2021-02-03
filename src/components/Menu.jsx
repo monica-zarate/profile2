85cc7826b4f720a1ddc42a040a38761abc0bb216
Monica Zarate <monicazaratecortes@gmail.com>
2021-02-03 14:33:28 -0800

    Styled User and Menu Components for mobile device. Still a wip.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/main.css";
+import User from "./User";
+import about from "../assets/icons/about.svg";
+import edu from "../assets/icons/edu.svg";
+import skills from "../assets/icons/tech.svg";
+import projects from "../assets/icons/projects.svg";
+import contact from "../assets/icons/contact.svg";
 
 function Menu () {
     return(
-        <div>Menu Component</div>
+        
+        <div className="menu">
+            <User/>
+            <ul className="menu__list">
+                <li className="menu__item-wrapper">
+                    <Link className="menu__link" to={`/`}>
+                        <img className="menu__img" src={about} alt=""/>
+                        <h3 className="menu__title">About</h3>
+                    </Link>
+                </li>
+                <li className="menu__item-wrapper">
+                    <Link className="menu__link" to={`/education`}>
+                        <img className="menu__img" src={edu} alt=""/>
+                        <h3 className="menu__title">Education</h3>
+                    </Link>
+                </li>
+                <li className="menu__item-wrapper">
+                    <Link className="menu__link" to={`/skills`}>
+                        <img className="menu__img" src={skills} alt=""/>
+                        <h3 className="menu__title">Skills</h3>
+                    </Link>
+                </li>
+                <li className="menu__item-wrapper">
+                    <Link className="menu__link" to={`/projects`}>
+                        <img className="menu__img" src={projects} alt=""/>
+                        <h3 className="menu__title">Projects</h3>
+                    </Link>
+                </li>
+                <li className="menu__item-wrapper">
+                    <Link className="menu__link" to={`/contact`}>
+                        <img className="menu__img" src={contact} alt=""/>
+                        <h3 className="menu__title">Contact</h3>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     )
 }
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./styles/main.css";
+import user from "../assets/imgs/user.png"
 
 function User () {
     return(
-        <div>User Component</div>
+        <div className="user">
+            <img className="user__icon" src={user} alt=""/>
+            <h1 className="user__title">Monica Zarate</h1>
+            <h2 className="user__subtitle">Web Developer</h2>
+        </div>
     )
 }
+
+export default User;
