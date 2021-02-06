57eb8768982b266b4e791710a0533fa67aafdf2a
Monica Zarate <monicazaratecortes@gmail.com>
2021-02-05 18:39:34 -0800

    Added dummie content and styled Projects Component for mobile device.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Nav from "./Nav";
 import "./styles/main.css";
 
 function Projects () {
     return(
-        <div>Portfolio Component</div>
+        <div className="component-wrapper">
+            <div className="projects">
+                <h2 className="projects__title">Projects</h2>
+                <ul className="projects__list">
+                    <li className="projects__item-wrapper">
+                        <h3 className="projects__item-title">Happy House</h3>
+                        <p className="projects__type">BrainStation Capstone Project</p>
+                        <button className="projects__btn">Read More</button>
+                    </li>
+                </ul>
+            </div>
+            <Nav/>
+        </div>
     )
 }
 
