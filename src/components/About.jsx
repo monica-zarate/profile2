a4b98dbf53b6f5b379c4f8bae94bde0fe7686854
Monica Zarate <monicazaratecortes@gmail.com>
2021-02-11 13:48:32 -0800

    Styled About Component for tablet and desktop devices.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
                 <p className="about__p">I'm a fullstack web developer. Highly analytical and detailed oriented. Vancouver, BC-based.</p>
                 <p className="about__p">I cultivated curiosity and analytical thinking from a young age mainly by
                 reading books. I’m a detail-oriented individual and I like to challenge myself by embarking on new projects to practice self-taught skills.</p>
-                <p className="about__p">My professional career started working in employee-focused roles. Now I’m excited to transition to a software development role and showcase my creativity by making a positive impact on people’s lives.</p>
+                <p className="about__p">My professional career started working in employee-focused roles. I have transitioned to a software development role to showcase my creativity by making a positive impact on people’s lives.</p>
                 <p className="about__p">I believe software is key in any industry and provides the opportunity for thrilling career development.</p>
             </div>
             <Nav/>
